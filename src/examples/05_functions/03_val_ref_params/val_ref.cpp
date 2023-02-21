5ad32d3758615748c73e83fdc2edad55d4a7008a
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-02-21 23:53:37 +0000

    const ref param

--- src/examples/05_functions/03_val_ref_params/val_ref.cpp
+++ src/examples/05_functions/03_val_ref_params/val_ref.cpp
@@ -9,3 +9,9 @@ void ref_params(int &num)
 {
     num = 20;
 }
+
+void ref_const_params(const int &num)//read only access to num
+{
+    //num = 20; we can't modify;compile error generated
+    //cout<<num;//but we can read the value
+}
