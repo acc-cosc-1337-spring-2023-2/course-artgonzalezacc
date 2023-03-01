be60f8766e029c812bf9daee3cc6e9e4e80c29d7
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-03-01 01:04:01 +0000

    class constructors,  public private func and vars

--- src/examples/08_classes/01_bank/checking_account.h
+++ src/examples/08_classes/01_bank/checking_account.h
@@ -1,1 +1,13 @@
 //checking_account.h
+#include<stdlib.h>//for rand() function
+
+class CheckingAccount
+{
+public:
+    CheckingAccount(){get_balance_from_db();}
+    CheckingAccount(int b) : balance(b){/*empty function code block*/}
+    int get_balance(){return balance;}
+private:
+    int balance{0};//explicitly initialize to zero
+    void get_balance_from_db();
+};
--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,6 +1,18 @@
+#include<iostream>
+#include<time.h>
+#include "checking_account.h"
+
+using std::cout;
 
 int main()
 {
+	srand(time(NULL));
+
+	CheckingAccount account;//cust 0
+	cout<<account.get_balance()<<"\n";
+
+	CheckingAccount account1;//cust 1
+	cout<<account1.get_balance()<<"\n";
 
 	return 0;
 }
