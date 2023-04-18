49521fd9a7b29d140e9d16c066c49bddfa43a645
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-04-18 23:22:58 +0000

    Use constructor destructor - manage dynamic memory

--- src/examples/11_memory/06_vector/my_vector.cpp
+++ src/examples/11_memory/06_vector/my_vector.cpp
@@ -1,4 +1,22 @@
 #include "my_vector.h"
 
 //
+using std::cout;
 
+Vector::Vector(int s)
+ : capacity{s}, elements{new int[s]}
+{
+    cout<<"created new memory at "<<elements<<"\n";
+}
+
+Vector::~Vector()
+{
+    cout<<"delete the memory at"<<elements<<"\n";
+    delete[] elements;
+}
+
+//NOT A CLASS FUNCTION; THESE ARE FREE FUNCTIONS
+void use_vector()
+{  
+    Vector v(3);
+}
--- src/examples/11_memory/06_vector/my_vector.h
+++ src/examples/11_memory/06_vector/my_vector.h
@@ -1,1 +1,23 @@
 //
+#include<iostream>
+
+#ifndef VECTOR_H
+#define VECTOR_H
+
+class Vector
+{
+public:
+    Vector(int s);
+    int Size()const{return size;}
+    int Capacity()const{return capacity;}
+    ~Vector();
+private:
+    int* elements;//holds the dynamic list on the heap
+    int size{0};//current size of the list; doubles as the current_index
+    int capacity;//total available slots for elements    
+};
+
+#endif
+
+//NOT A CLASS FUNCTION; THESE ARE FREE FUNCTIONS
+void use_vector();
