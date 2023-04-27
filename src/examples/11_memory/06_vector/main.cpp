5c3c4593bc797ffcfff47bb50948a96796bed9b2
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-04-27 23:11:02 +0000

    template function

--- src/examples/11_memory/06_vector/main.cpp
+++ src/examples/11_memory/06_vector/main.cpp
@@ -1,15 +1,25 @@
 #include "my_vector.h"
+#include<string>
 
-int main()
+using std::cout; using std::string;
+template<typename T>
+T add(T value1, T value2)
 {
-    //std::cout<<"load to the stack\n";
-    //use_vector();
-    //std::cout<<"unload from the stack\n";
-    int num = 5;
-    int& num_ref = num;
-    //we can't do this 5 = num;
-    //int& five = 5;
+    return value1 + value2;
+}
+int main()
+{   
+    auto result = add(5, 10);
+    cout<<"Result: "<<result<<"\n";
 
+    auto result1 = add(10.5, 5.3);
+    cout<<"Result: "<<result1<<"\n";
+
+    string v1 = "C++ ";
+    string v2 = "rocks";
+    auto result2 = add(v1, v2);
+    cout<<"Result: "<<result2<<"\n";
+    
     return 0;
 }
 
