5d17fcc689205122be5a4b6ebf2e8fa9f6ea4fa6
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2023-05-01 22:18:17 +0000

    char arrays

--- src/examples/10_arrays/06_arrays_char/arrays_char.h
+++ src/examples/10_arrays/06_arrays_char/arrays_char.h
@@ -1,1 +1,10 @@
 //char array example
+#include<iostream>
+#include<string>
+#include<memory>
+
+void use_char_null_terminated();
+void use_string_terminated();
+char* get_dynamic_memory(const int size);
+void delete_dynamic_char_memory(char* array);
+void limit_dynamic_char_mem_scope(const int size);
--- src/examples/10_arrays/06_arrays_char/main.cpp
+++ src/examples/10_arrays/06_arrays_char/main.cpp
@@ -1,8 +1,10 @@
-
+#include "arrays_char.h"
 
 int main()
 {
-	
+	//use_char_null_terminated();
+	//use_string_terminated();
+	limit_dynamic_char_mem_scope(11);
 
 	return 0;
 };
